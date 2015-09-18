f69a3eeaee734cb22d672a1663cbbef1e7057fca
SiGMan <sigman@ioupg.com>
2015-09-18 17:08:38 +0300

    checking extra args

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools.command.build_ext import build_ext
 VERSION = (0, 0, 3, 1)
 VERSION_STR = ".".join([str(x) for x in VERSION])
 
-COPT =  {'msvc': ['/Ox', '/Izstd/lib', '/DVERSION=\"\\\"%s\\\"\"' % VERSION_STR],
+COPT =  {'msvc': ['/Ox', '/Izstd\\lib', '/DVERSION=\"\\\"%s\\\"\"' % VERSION_STR],
      'mingw32' : ['-O3', '-Izstd/lib', '-march=native', '-DVERSION="%s"' % VERSION_STR],
      'clang' : ['-O3', '-Izstd/lib','-march=native', '-DVERSION="%s"' % VERSION_STR],
      'gcc' : ['-O3', '-Izstd/lib','-march=native', '-DVERSION="%s"' % VERSION_STR]}
@@ -17,11 +17,12 @@ class build_ext_subclass( build_ext ):
         if c in COPT:
            for e in self.extensions:
                e.extra_compile_args = COPT[c]
+        print "building with extra", e.extra_compiler_args
         build_ext.build_extensions(self)
 
 setup(
     name='zstd',
-    version=".".join([str(x) for x in VERSION]),
+    version=VERSION_STR,
     description="ZSTD Bindings for Python",
     author='Sergey Dryabzhinsky, Anton Stuk',
     author_email='sergey.dryabzhinsky@gmail.com',
@@ -42,8 +43,8 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
     ],
     cmdclass = {'build_ext': build_ext_subclass },
     setup_requires=["nose>=1.0"],
